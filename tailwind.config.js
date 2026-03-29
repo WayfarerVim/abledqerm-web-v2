11f5ef727beb5a9bc0e6692b79f1344a646c2f70
WayfarerVim <vimmannali@pm.me>
2026-03-29 09:08:23 +0100

    Update color palette and font families in config
    Removed some colors and added new color definitions along with font families.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,22 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        sage: '#DDE4DF',
-        dune: '#E5E1DA',
-        teal: '#2A7F7F',
-        charcoal: '#1E1E1E'
-      }
+        burgundy: '#6B3E3E',
+        burgundyDark: '#3D2626',
+        burgundyLight: '#8A5252',
+        gold: '#D4AF37',
+        goldLight: '#E8CC6A',
+        cream: '#F9F7F4',
+        creamDark: '#F0EBE5',
+        creamDeep: '#E6DDD4',
+        charcoal: '#3D3D3D',
+        midGray: '#6B6B6B',
+        lightGray: '#A0A0A0',
+      },
+      fontFamily: {
+        display: ['Cormorant Garamond', 'Georgia', 'serif'],
+        body: ['DM Sans', 'sans-serif'],
+      },
     }
   },
   plugins: []
