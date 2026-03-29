2298198cff242d964fa64a80bca879de88d40dd5
WayfarerVim <vimmannali@pm.me>
2026-03-29 09:15:36 +0100

    Add support for mdx files in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",   // Add mdx
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
