02574c197628ba9d20f189e9ed40195d9557db69
WayfarerVim <vimmannali@pm.me>
2026-03-29 05:18:41 +0100

    Create tailwind.config.js with theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        sage: '#DDE4DF',
+        dune: '#E5E1DA',
+        teal: '#2A7F7F',
+        charcoal: '#1E1E1E'
+      }
+    }
+  }
+}
